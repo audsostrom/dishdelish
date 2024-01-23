53091ae72624c7c02dee04008fda59ecd3c98e8d
audsostrom <audreyostrom64@gmail.com>
2024-01-22 16:53:27 -0800

    updating file hierarchy for routing

---
+++ src/app/components/ingredientsModal.js
@@ -0,0 +1,10 @@
+import './ingredientsModal.css';
+
+/** Modal that appears when you click on a button
+ * on the ingredient page on Figma */
+
+export default function ingredientsModal() {
+  return (
+   <div>ingredientsModal displays</div>
+  )
+}
