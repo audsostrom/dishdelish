66ca4d1d1cf4cce3c585ad8608fa2c1a80999f47
audsostrom <audreyostrom64@gmail.com>
2024-02-10 09:02:10 -0800

    figured out fetching

--- app/profile/page.js
+++ app/profile/page.js
@@ -6,12 +6,37 @@ import DefaultIcon from '../../assets/default-profile-icon.svg';
 import PencilIcon from '../../assets/pencil-icon.svg';
 import BannerImageNew from "../../assets/chef-ingredients.jpeg";
 import TextField from '@mui/material/TextField';
+import RecipeCard from '@/components/recipe-card/recipe-card';
+import exampleResponse from '../../data/spoonacular_response.json'
 
-function Profile() {
+
+// uncomment only when you need to, this is some dummy data so we don't over-use credits
+async function getData() {
+
+   /*
+   const res= await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?number=25&includeIngredients=tomato,cheese&apiKey=${process.env.SPOON_KEY}`
+    );
+  
+   if (!res.ok) {
+     // This will activate the closest `error.js` Error Boundary
+     throw new Error('Failed to fetch data')
+   }
+  
+   return res.json()
+   */
+  return exampleResponse;
+}
+
+
+async function Profile() {
+   const data = await getData();
+   console.log(data);
+
   let page = 1;
   return (
    <div className='profile-container'>
-      <div className='banner' style={{width: '100vw', height: '200px', position: 'relative'}}>
+      <div className='banner' style={{width: '100vw', height: '500px', position: 'relative'}}>
          <Image
             alt='banner'
             src={BannerImageNew}
@@ -30,22 +55,22 @@ function Profile() {
          <div className='option'>Your Reviews</div>
       </div>
       <hr></hr>
-               <div className='settings'>
-               <div className='email'>
-                  <div>email</div>
-                  <TextField id="outlined-basic"/>
-               </div>
-               <div className='display-name'>
-                  <div>display name</div>
-                  <TextField id="outlined-basic"/>
-               </div>
-               <div className='password'>
-                  <div>change password</div>
-                  <TextField id="outlined-basic" label="enter new password" type="password" />
-                  <TextField id="outlined-basic" label="confirm password" type="password" />
-               </div>
-      
+         <div className='settings'>
+            <div className='email'>
+               <div>email</div>
+               <TextField id="outlined-basic"/>
             </div>
+            <div className='display-name'>
+               <div>display name</div>
+               <TextField id="outlined-basic"/>
+            </div>
+            <div className='password'>
+               <div>change password</div>
+               <TextField id="outlined-basic" label="enter new password" type="password" />
+               <TextField id="outlined-basic" label="confirm password" type="password" />
+            </div>
+   
+         </div>
 
    </div>
   );
