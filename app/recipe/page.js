73cd1b472b935dcde08eac13481531b2d6cfb169
audsostrom <audreyostrom64@gmail.com>
2024-03-10 15:31:54 -0700

    more linting

--- app/recipe/page.js
+++ app/recipe/page.js
@@ -1,14 +1,21 @@
-// Import necessary dependencies and components
-import Link from 'next/link';
 import './recipe.css';
 import {createSavedRecipe} from '../db';
 import Image from 'next/image';
 import {Star, Favorite} from '@mui/icons-material';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-async function getData(recipeId) {
+/**
+ * The function `getData` fetches recipe information from the Spoonacular API using the provided recipe
+ * ID.
+ * @param recipeId - The `recipeId` parameter is the unique identifier of a recipe.
+ * @return {Promise} The `getData` function is returning a Promise that resolves to the JSON data fetched from
+ * the Spoonacular API for the specified `recipeId`. If the fetch operation is successful, the function
+ * returns the JSON data. If there is an error during the fetch operation (when `res.ok` is false), the
+ * function throws an error with the message 'Failed to fetch data'.
+ */
+export default async function getData(recipeId) {
 	const res = await fetch(
-		`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${process.env.SPOON_KEY}`
+		`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${process.env.SPOON_KEY}`,
 	);
 
 	if (!res.ok) {
@@ -18,8 +25,11 @@ async function getData(recipeId) {
 	return res.json();
 }
 
-
-// searchParams: gives recipes id and whether or not it's been favorited already
+/**
+ * @param {Object} searchParams - contains the search queries
+ * (i.e. whether or not a recipe has been favorited)
+ * @return {*} – Renders the Profile page
+ */
 async function RecipeInfo({searchParams}) {
 	console.log(searchParams['id']);
 	const data = await getData(searchParams['id']);
@@ -39,19 +49,28 @@ async function RecipeInfo({searchParams}) {
 	const email = '1234@gmail.com'; // change later, just to make demo quick
 
 	// very critical to use server here!
+	/**
+   * Used to save a recipe as a favorite for a specific user.
+   */
 	const handleFavorite = async () => {
 		'use server';
 		createSavedRecipe(email, data, favorited);
 	};
 
-	function getSummary() {
-		return {__html: data['summary']};
-	}
-
+	/**
+   * The function getInstructions returns the instructions stored in the data object as HTML.
+   * @return An object with a property named `__html` containing the value of `data['instructions']`.
+   */
 	function getInstructions() {
 		return {__html: data['instructions']};
 	}
 
+	/**
+   * The function `getExperienceLevel` determines a recipes's experience level
+   * based on the amount of time it takes to make it
+   * @param {Number} time - Time it takes to make a recipe in minutes
+   * @return {String} - Either 'beginner', 'medium', or 'hard'
+   */
 	function getExperienceLevel(time) {
 		if (time <= 45) {
 			return 'beginner';
@@ -63,71 +82,81 @@ async function RecipeInfo({searchParams}) {
 	}
 
 	const myArray = [];
-	for (let i = 0; i < (data['spoonacularScore'] / 20); i++) {
+	for (let i = 0; i < data['spoonacularScore'] / 20; i++) {
 		myArray.push(i);
 	}
 
-
 	return (
-		<div className='recipe-container'>
-			<div className='top-row'>
-				<div className='title'>Results &#62; {data['title']}</div>
-				<form className='favorite-button' action={handleFavorite}>
-
+		<div className="recipe-container">
+			<div className="top-row">
+				<div className="title">Results &#62; {data['title']}</div>
+				<form className="favorite-button" action={handleFavorite}>
 					<div style={{fontSize: '18px'}}>Favorite</div>
-					{!favorited && <button className='button' type="submit"><FavoriteBorderIcon style={{width: '20', height: '20'}}/></button>}
-					{favorited && <button className='button' type="submit"><Favorite style={{width: '20', height: '20'}}/></button>}
+					{!favorited && (
+						<button className="button" type="submit">
+							<FavoriteBorderIcon style={{width: '20', height: '20'}} />
+						</button>
+					)}
+					{favorited && (
+						<button className="button" type="submit">
+							<Favorite style={{width: '20', height: '20'}} />
+						</button>
+					)}
 				</form>
-
 			</div>
 			{/** has to be an action in order to use server-side functionality */}
-			<div className='body'>
-				<Image alt='recipe-photo' className='big-card-image' width='300' height='300' style={{objectFit: 'contain'}} src={data['image']}/>
+			<div className="body">
+				<Image
+					alt="recipe-photo"
+					className="big-card-image"
+					width="300"
+					height="300"
+					style={{objectFit: 'contain'}}
+					src={data['image']}
+				/>
 				<div>
-					<div className='big-title'>{data['title']}</div>
+					<div className="big-title">{data['title']}</div>
 					<div>
-						{
-							myArray.map((i) =>
-								<Star key={'star' + i}/>
-
-							)
-						}
+						{myArray.map((i) => (
+							<Star key={'star' + i} />
+						))}
 					</div>
 
-					<div className='experience'>experience level: {getExperienceLevel(data['readyInMinutes'])}</div>
-					<div className='diets'>
+					<div className="experience">
+            experience level: {getExperienceLevel(data['readyInMinutes'])}
+					</div>
+					<div className="diets">
 						<span>diets: </span>
-						{
-							data['diets'].map((diet, i) =>
-								((i == data['diets'].length - 1) ? <span key={'diet' + i}>{diet}</span> : <span key={'diet' + i}>{diet}, </span>)
-							)
-						}
+						{data['diets'].map((diet, i) =>
+							i == data['diets'].length - 1 ? (
+								<span key={'diet' + i}>{diet}</span>
+							) : (
+								<span key={'diet' + i}>{diet}, </span>
+							),
+						)}
 					</div>
-					<div className='cuisines'>
+					<div className="cuisines">
 						<span>cuisines: </span>
 						{data['cuisines'].length == 0 && <span>n/a</span>}
-						{
-							data['cuisines'].map((diet, i) =>
-								((i == data['diets'].length - 1) ? <span key={'cuisine' + i}>{diet}</span> : <span key={'cuisine' + i}>{diet}, </span>)
-							)
-						}
+						{data['cuisines'].map((diet, i) =>
+							i == data['diets'].length - 1 ? (
+								<span key={'cuisine' + i}>{diet}</span>
+							) : (
+								<span key={'cuisine' + i}>{diet}, </span>
+							),
+						)}
 					</div>
-
 				</div>
-
 			</div>
 			<hr></hr>
-			<div className='lower-part'>
-				<div className='options'>
+			<div className="lower-part">
+				<div className="options">
 					<div>Instructions</div>
 					<div>Additonal Information</div>
 				</div>
 				{page == 1 && <div dangerouslySetInnerHTML={getInstructions()}></div>}
 				{page == 2 && <div>Hello</div>}
-
 			</div>
 		</div>
 	);
 }
-
-export default RecipeInfo;
