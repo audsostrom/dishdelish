6e3c12ba9612b59fcbed04fa6a3996038c9ab14b
audsostrom <audreyostrom64@gmail.com>
2024-03-11 13:56:39 -0700

    everything works now

--- app/search-results/[id]/page.js
+++ app/search-results/[id]/page.js
@@ -17,19 +17,21 @@ async function getData(id) {
 	'use server';
 	const preferences = await getPreferences(id);
 	const userIngredients = preferences['ingredients'].join(',+');
+	console.log(preferences['intolerances'])
 	// const userCuisines = preferences['diets'].join(',');
 	// const userIntolerances = preferences['intolerances'].join(',');
 	console.log('ingredients', userIngredients);
 	const res = await fetch(
-		`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${userIngredients}&apiKey=${process.env.SPOON_KEY}`
+		`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${userIngredients}&number=1&&apiKey=${process.env.SPOON_KEY}`
 	);
 
 	if (!res.ok) {
 		// This will activate the closest `error.js` Error Boundary
 		throw new Error('Failed to fetch data');
 	}
-	const response = await res.json();
-	let ingredientResponse, ingredientPromise;
+	let response = await res.json();
+	console.log('respone', response)
+	let ingredientResponse, ingredientPromise, ingredientAisles, ingredientNames;
 	for (let i = 0; i < response.length; i++) {
 		ingredientPromise = await fetch(
 			`https://api.spoonacular.com/recipes/${response[i]['id']}/information?apiKey=${process.env.SPOON_KEY}`
@@ -39,7 +41,11 @@ async function getData(id) {
 			throw new Error('Failed to fetch data');
 		}
 		ingredientResponse = await ingredientPromise.json();
-		console.log(i, ingredientResponse)
+		// console.log(i, ingredientResponse['extendedIngredients'], typeof ingredientResponse['extendedIngredients']);
+		ingredientAisles = ingredientResponse['extendedIngredients'].map(a => a['aisle']?.toLowerCase());
+		ingredientNames = ingredientResponse['extendedIngredients'].map(a => a['name']?.toLowerCase());
+		console.log(ingredientAisles, 'aisles')
+		// console.log(ingredientAisles, 'aisles')
 		response[i]['readyInMinutes'] = ingredientResponse['readyInMinutes'];
 		response[i]['cuisines'] = ingredientResponse['cuisines'];
 		response[i]['diets'] = ingredientResponse['diets'];
@@ -50,39 +56,57 @@ async function getData(id) {
 
 		// paleo, vegan, vegetarian, dairy allergies and whole 30 diets can't eat dairy
 		if ((preferences['diets'].includes('paleo') || preferences['diets'].includes('vegan') || preferences['diets'].includes('vegetarian') || preferences['ingredients'].includes('whole 30') || preferences['intolerances'].includes('Dairy')) && ingredientResponse['dairyFree'] == false) {
-			response.filter(item => item !== response[i]);	
+			response = response.filter(item => item != response[i]);	
 			continue;	
 		}
 		// not vegan
 		if (preferences['ingredients'].includes('vegan') && ingredientResponse['vegan'] == false) {
-			response.filter(item => item !== response[i]);
+			response = response.filter(item => item != response[i]);
 			continue;
 		}
 		// not vegetarian
 		if (preferences['ingredients'].includes('vegetarian') && ingredientResponse['vegetarian'] == false) {
-			response.filter(item => item !== response[i]);
+			response = response.filter(item => item != response[i]);
 			continue;
 		}
 		// not gluten-free
 		if (preferences['intolerances'].includes('Gluten') && ingredientResponse['glutenFree'] == false) {
-			response.filter(item => item !== response[i]);
+			response = response.filter(item => item != response[i]);
 			continue;
 		}
 		// not dairy-free
 		if (preferences['intolerances'].includes('Dairy') && ingredientResponse['dairyFree'] == false) {
-			response.filter(item => item !== response[i]);
+			response = response.filter(item => item != response[i]);
+			continue;
+		}
+		// no fish if you're allergic to shell fish or fish
+		if ((preferences['intolerances'].includes('Seafood') || preferences['intolerances'].includes('shellfish') ) && (ingredientAisles.includes('seafood') ||  ingredientAisles.includes('fish'))) {
+			response = response.filter(item => item != response[i]);
+			continue;
+		}
+		// no peanuts if you're allergic to shell fish or fish
+		console.log(ingredientNames.includes('nut'), ingredientAisles.some((str) => str.includes('nut')))
+		if (preferences['intolerances'].includes('Peanuts') && (ingredientNames.some((str) => str.includes('nut')) ||  ingredientAisles.some((str) => str.includes('nut')))) {
+			response = response.filter(item => item != response[i]);
+			console.log(response, 'hi');
+			continue;
+		}
+		// no alcohol if you don't like it
+		if (preferences['intolerances'].includes('Alcohol') && (ingredientAisles.includes('alcohol'))) {
+			response = response.filter(item => item != response[i]);
 			continue;
 		}
 		// not diet compliant
 		if (!preferences['diets'].some((elem)=> ingredientResponse['diets'].includes(elem))) {
-			response.filter(item => item !== response[i]);
+			response = response.filter(item => item != response[i]);
 			continue;
 		}
 		// doesn't match cuisines
-		if (ingredientResponse['cuisines'] != [] && !preferences['cuisinse'].some((elem)=> ingredientResponse['cuisines'].includes(elem))) {
-			response.filter(item => item !== response[i]);
+		if (ingredientResponse['cuisines'] != [] && !preferences['cuisine'].some((elem)=> ingredientResponse['cuisines'].includes(elem))) {
+			response = response.filter(item => item != response[i]);
 			continue;
 		}
+
 	}
 	return response;
 }
@@ -124,7 +148,11 @@ async function Results({params}) {
 
 				<div className='showing-results'>Showing {data.length} Results</div>
 			</div>
-			<div className='recipe-cards-wrapper'>
+			{
+				data.length == 0 ? 
+				<div>No recipes found! Try with more or different ingredients, or with different ingredients.</div>
+				:
+				<div className='recipe-cards-wrapper'>
 				{
 					data.map((item, i) =>
 					// this redirects you to specific recipe
@@ -146,6 +174,7 @@ async function Results({params}) {
 					)
 				}
 			</div>
+			}
 
 		</div>
 	);
